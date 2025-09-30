991b1529e3ddca35b6137d0719404bfefcd743ee
nikorr0 <46054489+nikorr0@users.noreply.github.com>
2025-09-30 16:00:49 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+from PySide6.QtCore import Slot
+from PySide6.QtGui import QPixmap
+from PySide6.QtWidgets import (QApplication, QDialog, QLabel,
+                               QPushButton, QGraphicsScene, 
+                               QWidget, QMainWindow, QColorDialog,
+                               QTableWidgetItem)
+
+
