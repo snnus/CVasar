e54ab24adbd23202f6fee0e34a1895b51043dae9
nikorr0 <46054489+nikorr0@users.noreply.github.com>
2025-10-08 13:38:11 +0500

    Add upload image function

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from PySide6.QtGui import QPixmap, QPalette
 from PySide6.QtWidgets import (QApplication, QDialog, QLabel,
                                QPushButton, QGraphicsScene, 
                                QWidget, QMainWindow, QColorDialog,
-                               QTableWidgetItem)
+                               QTableWidgetItem, QFileDialog)
 from ui_mainwindow import Ui_Main_window
 import sys
+from PIL import Image
+import numpy as np
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -13,8 +15,24 @@ class MainWindow(QMainWindow):
         self.ui = Ui_Main_window()
         self.ui.setupUi(self)
         
+        self.original_image = None
+        
         self.ui.fill_brightness_color_button.clicked.connect(self.color_selection)
+        self.ui.upload_button.clicked.connect(self.upload_image)
 
+
+    def upload_image(self):
+        file_name, _ = QFileDialog.getOpenFileName(self, "Open Image", "", "Only Images (*.png *.jpg *.jpeg *.bmp)")
+        
+        scene = QGraphicsScene()
+        pixmap = QPixmap(file_name)
+        scene.addPixmap(pixmap)
+        
+        self.original_image = np.asarray(Image.open(file_name))
+
+        self.ui.image_View.setScene(scene)
+    
+    
     def color_selection(self):
         button = self.sender()
         background_color = button.palette().color(QPalette.Button)
